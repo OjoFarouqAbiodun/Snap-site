4f7c32b3f3ec753541ad256d313cb670b0f0711f
Ojo Farouq Abiodun <harbyodunojo@gmail.com>
2025-08-31 06:34:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const showMenu = document.querySelector('.showMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const mobileMenu = document.querySelector('.mobile');
+const overlay = document.querySelector('.overlay');
+
+function openMenu() {
+  mobileMenu.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeMenuFunc() {
+  mobileMenu.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+showMenu.addEventListener('click', openMenu);
+
+closeMenu.addEventListener('click', closeMenuFunc);
+
+overlay.addEventListener('click', closeMenuFunc);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+    closeMenuFunc();
+  }
+});
+// Select dropdown triggers
+const featureToggle = document.querySelector("#feature > a");
+const companyToggle = document.querySelector("#company > a");
+
+// Select the dropdown content and arrows
+const mobileFeatures = document.querySelector(".mobile-features-links");
+const mobileCompany = document.querySelector(".mobile-company-links");
+
+const featureArrow = document.querySelector("#feature img");
+const companyArrow = document.querySelector("#company img");
+
+// Function to toggle dropdowns
+function toggleDropdown(dropdown, arrow) {
+  dropdown.classList.toggle("show-dropdown");
+
+  if (dropdown.classList.contains("show-dropdown")) {
+    arrow.src = "./images/icon-arrow-up.svg";
+  } else {
+    arrow.src = "./images/icon-arrow-down.svg";
+  }
+}
+
+// Features toggle
+featureToggle.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevent link navigation
+  toggleDropdown(mobileFeatures, featureArrow);
+});
+
+// Company toggle
+companyToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleDropdown(mobileCompany, companyArrow);
+});
